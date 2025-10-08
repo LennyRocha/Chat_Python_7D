b2b2ca3c5f86ec59748ee2c9de19112d023713d2
Daniel-Aguilar-Dev <20233tn088@utez.edu.mx>
2025-10-07 18:33:03 -0600

    codigo hashing simetrico agregado

--- chat_python_v1/chat_server.py
+++ chat_python_v1/chat_server.py
@@ -1,11 +1,38 @@
 #python -m http.server 8000
 #pip install websockets
 #python chat_server.py
+
+#HASHING SIMETRICO
+
+
 import asyncio
 import websockets
+# Librerias para hashing simetrico
+import hmac
+import hashlib
+
+IP_SERVIDOR = "192.168.109.211"
+PUERTO = 5001
+# Clave secreta compartida. cliente y servidor deben tenerla igual
+CLAVE_SECRETA = b"clave_super_secreta" # Debe ser bytes, (b"")
 
 canales = {"general": set()}
 nombres = {}
+
+# Función para verificar la integridad del mensaje con HMAC
+def verificar_hash(mensaje, hash_recibido):
+    #Verifica que el hash HMAC coincida con el mensaje recibido
+    hash_calculado = hmac.new(CLAVE_SECRETA, mensaje.encode(), hashlib.sha256).hexdigest()
+    print(f"\n Verificando mensaje: '{mensaje}'") # imprime el mensaje recibido
+    print(f"Hash recibido: {hash_recibido}") # imprime el hash recibido
+    print(f"Hash calculado: {hash_calculado}") # imprime el hash calculado
+    # Comparar y mostrar resultado
+    if hmac.compare_digest(hash_calculado, hash_recibido):
+        print("El hash coincide. El mensaje es íntegro y auténtico.")
+        return True
+    else:
+        print("El hash NO coincide. El mensaje pudo ser alterado o la clave es incorrecta.")
+        return False
 
 async def manejar_cliente(websocket):
     try:
@@ -20,7 +47,15 @@ async def manejar_cliente(websocket):
         await actualizar_usuarios(canal)
 
         async for mensaje in websocket:
-            await broadcast(f"{nombre}: {mensaje}", canal, websocket)
+            try:
+                # Se espera que el mensaje llegue como "texto|hash"
+                texto, hash_recibido = mensaje.split("|")
+                if verificar_hash(texto, hash_recibido):
+                    await broadcast(f"{nombre}: {texto}", canal, websocket)
+                else:
+                    await websocket.send("Hash inválido: posible manipulación del mensaje.")
+            except Exception as e:
+                await websocket.send(f"Error de formato o verificación: {e}")
     except websockets.ConnectionClosed:
         pass
     finally:
@@ -49,8 +84,8 @@ async def actualizar_usuarios(canal):
             pass
 
 async def main():
-    server = await websockets.serve(manejar_cliente, "192.168.0.13", 5001)
-    print("Servidor WebSocket con canales listo en ws://192.168.0.13:5001")
+    server = await websockets.serve(manejar_cliente, IP_SERVIDOR, PUERTO)
+    print("Servidor WebSocket con canales listo en ws://{IP_SERVIDOR}:{PUERTO}")
     await server.wait_closed()
 
 if __name__ == "__main__":
